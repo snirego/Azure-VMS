fc427a5ec56b86c603cb40dff5a5d4b7861a96cd
Snir <snirego75@gmail.com>
2022-12-21 11:52:36 +0200

    json blob commit

--- script.js
+++ script.js
@@ -13,7 +13,31 @@ var listOfTranscriptWithTimestamps = [];
 var listOfLabelsWithTimestamps = [];
 var listOfLabels = [];
 
-videoUploadInput.addEventListener('change', function(event) {
+// ----------------------------------------------< BLOB JSON START >-----------------------------------------------
+async function getBlob(accountName, containerName, blobName) {
+    // Replace {accountName} and {containerName} with your Azure Storage account name and container name, respectively
+    const storageUrl = `https://videoindexerstaccoun.blob.core.windows.net/insights-json/${blobName}`;
+
+    // Set the SAS token to allow you to access the blob.
+    // Replace {sasToken} with a valid SAS token for the blob.
+    const sasToken = '?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2022-12-21T17:40:48Z&st=2022-12-21T09:40:48Z&spr=https,http&sig=c6cUFUkFUjcAjth8v63ceEVpuD6mC4bNxLD1DZ9uo7U%3D';
+
+    // Send the GET request to the Azure Storage API to retrieve the blob
+    const response = await fetch(storageUrl);
+
+    // If the request was successful, return the blob data
+    if (response.ok) {
+        return await response.blob();
+    } else {
+        throw new Error(`Failed to retrieve blob: ${response.statusText}`);
+    }
+}
+
+
+// ----------------------------------------------< BLOB JSON END >-----------------------------------------------
+
+
+videoUploadInput.addEventListener('change', function (event) {
     // This function will be executed when a file is uploaded to the 'video-upload' input element
     const videoFile = event.target.files[0];
 
@@ -24,88 +48,99 @@ videoUploadInput.addEventListener('change', function(event) {
     player.onloadedmetadata = () => {
         player.play();
     };
-  });
+
+    // Call function(videoName) to get the JSON file from the video file
+    getBlob('videoindexerstaccoun', 'insights-json', videoFile.name + '_insights.json')
+        .then(blob => {
+            // The blob data is now stored in the "blob" variable
+            console.log(blob);
+        })
+        .catch(error => {
+            // An error occurred while trying to retrieve the blob
+        });
+
+});
 
 
-jsonUploadInput.addEventListener('change', function(event) {
-    // This function will be executed when a file is uploaded to the 'json-upload' input element
-    const jsonFile = event.target.files[0];
+// jsonUploadInput.addEventListener('change', function(event) {
+//     // This function will be executed when a file is uploaded to the 'json-upload' input element
+//     const jsonFile = event.target.files[0];
 
-    // Create a new FileReader
-    const reader = new FileReader();
+//     // Create a new FileReader
+//     const reader = new FileReader();
 
-    // Add an event listener to the FileReader's 'load' event
-    reader.addEventListener('load', () => {
-    // The 'load' event is triggered when the file has been read successfully
-    // The file's contents can be accessed through the 'result' property of the FileReader object
-    jsonData = JSON.parse(reader.result);
-    
-    console.log(jsonData);
+//     // Add an event listener to the FileReader's 'load' event
+//     reader.addEventListener('load', () => {
+//     // The 'load' event is triggered when the file has been read successfully
+//     // The file's contents can be accessed through the 'result' property of the FileReader object
+//     jsonData = JSON.parse(reader.result);
 
-
-    // Transcript JSON data
-    var listOfTranscript = jsonData.videos[0].insights.transcript;
-
-    for (var i = 0; i < listOfTranscript.length; i++) 
-    {
-        // sentence text
-        var text = listOfTranscript[i].text;
-        var start = listOfTranscript[i].instances[0].start;
-        var end = listOfTranscript[i].instances[0].end;        
-
-        // Convert the timestamp string to a number of seconds
-        var startTimestampSeconds = parseInt(start.split(":")[0]) * 3600 +
-        parseInt(start.split(":")[1]) * 60 +
-        parseInt(start.split(":")[2].split(".")[0]) +
-        parseInt(start.split(":")[2].split(".")[1]) / 100;
-
-        var endTimestampSeconds = parseInt(end.split(":")[0]) * 3600 +
-        parseInt(end.split(":")[1]) * 60 +
-        parseInt(end.split(":")[2].split(".")[0]) +
-        parseInt(end.split(":")[2].split(".")[1]) / 100;
-
-        // console.log(text, startTimestampSeconds, endTimestampSeconds);
-
-        listOfTranscriptWithTimestamps.push([text, startTimestampSeconds, endTimestampSeconds]);
-    }
-    console.log(listOfTranscriptWithTimestamps)
+//     console.log(jsonData);
 
 
+//     // Transcript JSON data
+//     var listOfTranscript = jsonData.videos[0].insights.transcript;
 
-    // Labels JSON data
-    listOfLabels = jsonData.videos[0].insights.labels;
+//     for (var i = 0; i < listOfTranscript.length; i++) 
+//     {
+//         // sentence text
+//         var text = listOfTranscript[i].text;
+//         var start = listOfTranscript[i].instances[0].start;
+//         var end = listOfTranscript[i].instances[0].end;        
 
-    for (var i = 0; i < listOfLabels.length; i++)
-    {
-        var label = listOfLabels[i].name;
-        listOfLabelsWithTimestamps[i] = [];
+//         // Convert the timestamp string to a number of seconds
+//         var startTimestampSeconds = parseInt(start.split(":")[0]) * 3600 +
+//         parseInt(start.split(":")[1]) * 60 +
+//         parseInt(start.split(":")[2].split(".")[0]) +
+//         parseInt(start.split(":")[2].split(".")[1]) / 100;
 
-        for (var j = 0; j < listOfLabels[i].instances.length; j++)
-        {
-            var start = listOfLabels[i].instances[j].start;
-            var end = listOfLabels[i].instances[j].end;
+//         var endTimestampSeconds = parseInt(end.split(":")[0]) * 3600 +
+//         parseInt(end.split(":")[1]) * 60 +
+//         parseInt(end.split(":")[2].split(".")[0]) +
+//         parseInt(end.split(":")[2].split(".")[1]) / 100;
 
-            // Convert the timestamp string to a number of seconds
-            var startTimestampSeconds = parseInt(start.split(":")[0]) * 3600 +
-            parseInt(start.split(":")[1]) * 60 +
-            parseInt(start.split(":")[2].split(".")[0]) +
-            parseInt(start.split(":")[2].split(".")[1]) / 100;
+//         // console.log(text, startTimestampSeconds, endTimestampSeconds);
 
-            var endTimestampSeconds = parseInt(end.split(":")[0]) * 3600 +
-            parseInt(end.split(":")[1]) * 60 +
-            parseInt(end.split(":")[2].split(".")[0]) +
-            parseInt(end.split(":")[2].split(".")[1]) / 100;
+//         listOfTranscriptWithTimestamps.push([text, startTimestampSeconds, endTimestampSeconds]);
+//     }
+//     console.log(listOfTranscriptWithTimestamps)
 
-            console.log(label, startTimestampSeconds, endTimestampSeconds);
-            listOfLabelsWithTimestamps[i][j] = [startTimestampSeconds, endTimestampSeconds];
-        }
-    }
-    console.log(listOfLabelsWithTimestamps)
-  });
 
-  // Start reading the file
-  reader.readAsText(jsonFile);
-  });
+
+//     // Labels JSON data
+//     listOfLabels = jsonData.videos[0].insights.labels;
+
+//     for (var i = 0; i < listOfLabels.length; i++)
+//     {
+//         var label = listOfLabels[i].name;
+//         listOfLabelsWithTimestamps[i] = [];
+
+//         for (var j = 0; j < listOfLabels[i].instances.length; j++)
+//         {
+//             var start = listOfLabels[i].instances[j].start;
+//             var end = listOfLabels[i].instances[j].end;
+
+//             // Convert the timestamp string to a number of seconds
+//             var startTimestampSeconds = parseInt(start.split(":")[0]) * 3600 +
+//             parseInt(start.split(":")[1]) * 60 +
+//             parseInt(start.split(":")[2].split(".")[0]) +
+//             parseInt(start.split(":")[2].split(".")[1]) / 100;
+
+//             var endTimestampSeconds = parseInt(end.split(":")[0]) * 3600 +
+//             parseInt(end.split(":")[1]) * 60 +
+//             parseInt(end.split(":")[2].split(".")[0]) +
+//             parseInt(end.split(":")[2].split(".")[1]) / 100;
+
+//             console.log(label, startTimestampSeconds, endTimestampSeconds);
+//             listOfLabelsWithTimestamps[i][j] = [startTimestampSeconds, endTimestampSeconds];
+//         }
+//     }
+//     console.log(listOfLabelsWithTimestamps)
+//   });
+
+//   // Start reading the file
+//   reader.readAsText(jsonFile);
+//   });
 
 
 // ----------------------------------------------< FUNCTIONS >-----------------------------------------------
@@ -119,31 +154,28 @@ document.getElementById('famousPeople').addEventListener('click', () => {
 
     var listOfFamousPeople = jsonData.summarizedInsights.namedPeople;
 
-    
+
     console.log(famousPeoplePlaceholder.childElementCount)
 
-    if (famousPeoplePlaceholder.childElementCount > 0)
-    {
+    if (famousPeoplePlaceholder.childElementCount > 0) {
         // Delete all child nodes
-        while (famousPeoplePlaceholder.firstChild !== null) 
-        {
+        while (famousPeoplePlaceholder.firstChild !== null) {
             famousPeoplePlaceholder.removeChild(famousPeoplePlaceholder.firstChild);
         }
     }
 
-    for (var i = 0; i < listOfFamousPeople.length; i++) 
-    {
+    for (var i = 0; i < listOfFamousPeople.length; i++) {
         var textDiv = document.createElement('div');
         var linkDiv = document.createElement('a');
         var linkText = document.createTextNode(linkDiv.href = listOfFamousPeople[i].referenceUrl);
 
         linkDiv.appendChild(linkText);
-        
+
         famousPeoplePlaceholder.appendChild(textDiv);
         famousPeoplePlaceholder.appendChild(linkDiv);
 
         textDiv.innerText = "Celebrity Name: " + listOfFamousPeople[i].name;
-        
+
         // open link in new tab
         linkDiv.target = "_blank";
     }
@@ -158,42 +190,34 @@ document.getElementById('transcript').addEventListener('click', () => {
 });
 
 // Get the current time of the video player
-    player.addEventListener('timeupdate', () => {
-        var currentTime = player.currentTime;
-        // console.log(currentTime);
+player.addEventListener('timeupdate', () => {
+    var currentTime = player.currentTime;
+    // console.log(currentTime);
 
-        for (var i = 0; i < listOfTranscriptWithTimestamps.length; i++) 
-        {
-            if (currentTime >= listOfTranscriptWithTimestamps[i][1] && currentTime <= listOfTranscriptWithTimestamps[i][2]) 
-            {
-                // console.log(listOfTranscriptWithTimestamps[i][0]);
-                transcriptPlaceholder.innerText = listOfTranscriptWithTimestamps[i][0];
-            }
+    for (var i = 0; i < listOfTranscriptWithTimestamps.length; i++) {
+        if (currentTime >= listOfTranscriptWithTimestamps[i][1] && currentTime <= listOfTranscriptWithTimestamps[i][2]) {
+            // console.log(listOfTranscriptWithTimestamps[i][0]);
+            transcriptPlaceholder.innerText = listOfTranscriptWithTimestamps[i][0];
         }
+    }
 
-        for (var i = 0; i < listOfLabelsWithTimestamps.length; i++)
-        {
-            for (var j = 0; j < listOfLabelsWithTimestamps[i].length; j++)
-            {
-                if (currentTime >= listOfLabelsWithTimestamps[i][j][0] && currentTime <= listOfLabelsWithTimestamps[i][j][1])
-                {
-                    for (var k = 0; k < labelsPlaceholder.childElementCount; k++)
-                    {
-                        if (labelsPlaceholder.children[k].innerText === listOfLabels[i].name)
-                        {
-                            labelsPlaceholder.children[k].style.backgroundColor = 'blue';
-                            labelsPlaceholder.children[k].style.color = 'white';
-                        }
-                        else
-                        {
-                            labelsPlaceholder.children[k].style.backgroundColor = 'white';
-                            labelsPlaceholder.children[k].style.color = 'black';
-                        }
+    for (var i = 0; i < listOfLabelsWithTimestamps.length; i++) {
+        for (var j = 0; j < listOfLabelsWithTimestamps[i].length; j++) {
+            if (currentTime >= listOfLabelsWithTimestamps[i][j][0] && currentTime <= listOfLabelsWithTimestamps[i][j][1]) {
+                for (var k = 0; k < labelsPlaceholder.childElementCount; k++) {
+                    if (labelsPlaceholder.children[k].innerText === listOfLabels[i].name) {
+                        labelsPlaceholder.children[k].style.backgroundColor = 'blue';
+                        labelsPlaceholder.children[k].style.color = 'white';
+                    }
+                    else {
+                        labelsPlaceholder.children[k].style.backgroundColor = 'white';
+                        labelsPlaceholder.children[k].style.color = 'black';
                     }
                 }
             }
         }
-    });
+    }
+});
 
 // Add an event listener to the emotion button to display the data from the JSON file in the browser
 document.getElementById('labels').addEventListener('click', () => {
@@ -204,17 +228,14 @@ document.getElementById('labels').addEventListener('click', () => {
 
 
 
-    if (labelsPlaceholder.childElementCount > 0)
-    {
+    if (labelsPlaceholder.childElementCount > 0) {
         // Delete all child nodes
-        while (labelsPlaceholder.firstChild !== null)
-        {
+        while (labelsPlaceholder.firstChild !== null) {
             labelsPlaceholder.removeChild(labelsPlaceholder.firstChild);
         }
     }
 
-    for (var i = 0; i < listOfLabels.length; i++) 
-    {
+    for (var i = 0; i < listOfLabels.length; i++) {
         var textDiv = document.createElement('div');
 
         labelsPlaceholder.appendChild(textDiv);
@@ -230,41 +251,3 @@ document.getElementById('labels').addEventListener('click', () => {
 
 
 
-
-// // ----------------------------------------------< SERVER START >-----------------------------------------------
-// const { BlobServiceClient, StorageSharedKeyCredential } = require("@azure/storage-blob");
-
-// // Enter your storage account name and shared key
-// const account = "storage_account_name";
-// const accountKey = "storage_account_key";
-
-// // Use StorageSharedKeyCredential with storage account and account key
-// const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
-
-// // Use sharedKeyCredential to create a BlobServiceClient
-// const blobServiceClient = new BlobServiceClient(
-//   `https://${account}.blob.core.windows.net`,
-//   sharedKeyCredential
-// );
-
-// async function uploadToBlob() {
-//   // Get a reference to a container
-//   const containerName = "my-container";
-//   const containerClient = blobServiceClient.getContainerClient(containerName);
-
-//   // Create the container if it doesn't exist
-//   await containerClient.create();
-
-//   // Create a new block blob
-//   const blobName = "my-blob.txt";
-//   const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-
-//   // Upload data to the blob
-//   const data = "Hello, Azure Blob Storage!";
-//   const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
-//   console.log(`Uploaded block blob ${blobName} to container ${containerName}`);
-// }
-
-// uploadToBlob().catch(err => {
-//   console.error("An error occurred: ", err);
-// });
